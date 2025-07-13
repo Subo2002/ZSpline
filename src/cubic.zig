98ece7d3df07b3d49a16ccbb2a6abbbbfec12f4b
subo2002-Laptop <subocashell@gmail.com>
2025-07-13 17:31:34 +0100

    trying ot fix array init being wrong

--- src/cubic.zig
+++ src/cubic.zig
@@ -11,7 +11,7 @@ pub const CubicSpline = struct {
     p3: Vector2I,
 
     pub fn draw(c: *const CubicSpline, out_buffer: []Vector2I) []Vector2I {
-        var monotoneParts: []CubicSpline = [1]CubicSpline{.{
+        var monotoneParts: []CubicSpline = [1]CubicSpline{CubicSpline{
             .{ .p0 = .zero, .p1 = .zero, .p2 = .zero, .p3 = .zero },
         }} ** 5;
         monotoneParts = c.cutToMontone(monotoneParts);
