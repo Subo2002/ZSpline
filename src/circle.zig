ee9173d9ee4d518e1394a48464d8145bd9ff77a5
suboc <subocashell@gmail.com>
2025-08-04 21:30:50 +0100

    Added circle bresenham's alg

---
+++ src/circle.zig
@@ -0,0 +1,51 @@
+const Vector2I = @import("vector.zig").Vector2I;
+
+pub const Circle = struct {
+    p: Vector2I,
+    r: i16,
+
+    pub fn init(p: Vector2I, r: i16) Circle {
+        return .{ .p = p, .r = r };
+    }
+
+    pub fn draw(c: *const Circle, out_buffer: []Vector2I) []Vector2I {
+        var dx: i32 = 1;
+        var dy: i32 = 1 - 2 * c.r;
+
+        const ddx = 2;
+        const ddy = -2;
+
+        var e: i32 = dx + dy;
+        var e2: i32 = 0;
+        var pos: Vector2I = .{ .x = 0, .y = c.r };
+
+        var i: i16 = 0;
+        while (true) {
+            if (i == out_buffer.len)
+                break;
+
+            out_buffer[i] = c.p + pos;
+            out_buffer[i + 1] = c.p + .{ .x = -pos.x, .y = pos.y };
+            out_buffer[i + 2] = c.p + .{ .x = pos.x, .y = -pos.y };
+            out_buffer[i + 3] = c.p + .{ .x = -pos.x, .y = -pos.y };
+            i += 4;
+
+            if (pos.eql(.{ .x = c.r, .y = 0 }))
+                break;
+
+            e2 = 2 * e;
+            if (e2 >= dx) {
+                dx += ddx;
+                e += dx;
+                pos.x += 1;
+            }
+            if (e2 <= dy) {
+                dy += ddy;
+                e += dy;
+                pos.y += -1;
+            }
+        }
+
+        return out_buffer[0..i];
+    }
+};
--- src/root.zig
+++ src/root.zig
@@ -1,6 +1,7 @@
 pub const Line = @import("line.zig").Line;
-pub const CubicSpline = @import("cubic.zig").CubicSpline;
+pub const Circle = @import("circle.zig").Circle;
 pub const QuadSpline = @import("quad.zig").QuadSpline;
+pub const CubicSpline = @import("cubic.zig").CubicSpline;
 pub const Vector2 = @import("vector.zig").Vector2;
 pub const Vector2I = @import("vector.zig").Vector2I;
 pub const Vector2B = @import("vector.zig").Vector2B;
