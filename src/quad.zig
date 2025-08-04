cb143f40d71c6ba1d5e5c36340a823dde42f70d9
suboc <subocashell@gmail.com>
2025-08-04 21:08:39 +0100

    forgot to get type out of import

--- src/quad.zig
+++ src/quad.zig
@@ -1,7 +1,7 @@
 const Vector2 = @import("vector.zig").Vector2;
 const Vector2I = @import("vector.zig").Vector2I;
 const Vector2B = @import("vector.zig").Vector2B;
-const Line = @import("line.zig");
+const Line = @import("line.zig").Line;
 const std = @import("std");
 
 pub const QuadSpline = struct {
