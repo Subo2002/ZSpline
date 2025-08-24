152567fb5ba660dec204d1bc87944ed0c10724ce
subo2002-Laptop <subocashell@gmail.com>
2025-08-24 14:45:14 +0100

    array index int cast needed

--- src/floodFill.zig
+++ src/floodFill.zig
@@ -4,7 +4,7 @@ const std = @import("std");
 //last in first out -> width search -> flood <- less memory required, know want to hit everything
 pub const FloodFill = struct {
     pub fn fill(start: Vector2I, target: u16, space: []u16, width: u16, buffer: []Vector2I) void {
-        if (space[start.y * width + start.x] == target) return;
+        if (space[@intCast(start.y * width + start.x)] == target) return;
         buffer[0] = start;
         var no: u16 = 1; //start
         var cur: u16 = 0;
@@ -20,7 +20,7 @@ pub const FloodFill = struct {
                     3 => Vector2I{ .x = -1, .y = 0 },
                 };
                 const test_pos = pos.add(offset);
-                if (space[test_pos.y * width + test_pos.x] == target) continue;
+                if (space[@intCast(test_pos.y * width + test_pos.x)] == target) continue;
                 buffer[no] = test_pos;
                 no += 1;
             }
