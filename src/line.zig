ce17fe476c2a49192c0a66f969f3896f02317eec
subo2002-Laptop <subocashell@gmail.com>
2026-03-28 17:10:46 +0000

    over-optimized Line.draw. It's now branchless, and doesn't use imul in the loop for the branchless comps by using bit masks and &. NEVERMIND, turns out llvm just compiles it back to the original branched approach.
    Have to update ZSMath for project that uses both librarys...

--- src/line.zig
+++ src/line.zig
@@ -5,43 +5,84 @@ pub const Line = struct {
     q: Vector2I,
 
     pub fn draw(c: *const Line, out_buffer: []Vector2I) []Vector2I {
-        var i: u32 = 0;
+        var i: usize = 0;
 
         const p: Vector2I = c.p;
         const q: Vector2I = c.q;
 
-        const sx: i32 = if (q.x > p.x) 1 else -1;
-        const sy: i32 = if (q.y > p.y) 1 else -1;
+        const sx: i2 = if (q.x > p.x) 1 else -1;
+        const sy: i2 = if (q.y > p.y) 1 else -1;
         const dx: i32 = -sy * (q.y - p.y);
         const dy: i32 = sx * (q.x - p.x);
         var e: i32 = dx + dy;
         var e2: i32 = 0;
         var r: Vector2I = p;
+        var cx: i32 = 0;
+        var cy: i32 = 0;
+        const len = out_buffer.len;
+
+        out_buffer[i] = p;
+        i += 1;
 
         while (true) {
-            if (i == out_buffer.len)
-                break;
+            e2 = e << 1; //maybe
+            cx = -@as(i32, @intFromBool(e2 >= dx)); //0 -> 0x0000, 1 -> 0xFFFF bit masks
+            cy = -@as(i32, @intFromBool(e2 <= dy));
+            e += (cx & dx) + (cy & dy); //using bit masks and & to avoid * by 0 or 1, which compiles to using imul, even if cx cy are u1
+            r.x += cx & sx;
+            r.y += cy & sy;
 
             out_buffer[i] = r;
             i += 1;
-
-            if (r.x == q.x and r.y == q.y)
+            if (i == len) {
+                @branchHint(.cold);
                 break;
-
-            e2 = 2 * e;
-            if (e2 >= dx) {
-                e += dx;
-                r.x += sx;
             }
-            if (e2 <= dy) {
-                e += dy;
-                r.y += sy;
-            }
+            if (r.x == q.x and r.y == q.y) break;
         }
 
         return out_buffer[0..i];
     }
 };
+
+//old, but not any worse actually
+//pub fn draw(c: *const Line, out_buffer: []Vector2I) []Vector2I {
+//    var i: u32 = 0;
+//
+//    const p: Vector2I = c.p;
+//    const q: Vector2I = c.q;
+//
+//    const sx: i32 = if (q.x > p.x) 1 else -1;
+//    const sy: i32 = if (q.y > p.y) 1 else -1;
+//    const dx: i32 = -sy * (q.y - p.y);
+//    const dy: i32 = sx * (q.x - p.x);
+//    var e: i32 = dx + dy;
+//    var e2: i32 = 0;
+//    var r: Vector2I = p;
+//
+//    while (true) {
+//        if (i == out_buffer.len)
+//            break;
+//
+//        out_buffer[i] = r;
+//        i += 1;
+//
+//        if (r.x == q.x and r.y == q.y)
+//            break;
+//
+//        e2 = 2 * e;
+//        if (e2 >= dx) {
+//            e += dx;
+//            r.x += sx;
+//        }
+//        if (e2 <= dy) {
+//            e += dy;
+//            r.y += sy;
+//        }
+//    }
+//
+//    return out_buffer[0..i];
+//}
 
 test "line works" {
     const line = Line{
